41778ff980a3b0eb83fe8953cb21e20cbbd22643
Liviu Hariton <liviu.hariton@gmail.com>
2024-07-01 22:02:56 +0300

    Login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,33 +11,41 @@
                                 <div class="col-lg-6 bg-primary content-left">
                                     <div class="p-3">
                                         <img src="{{ secure_asset('assets/images/logo-white-simple.png') }}" alt="TaskTrail">
+
                                         <p>Log in to blaze your trail with TaskTrail!</p>
-                                        <form method="post">
+
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
+
                                             <div class="row">
                                                 <div class="col-lg-12">
-                                                    <div class="floating-label form-group">
-                                                        <input class="floating-input form-control" type="email"
-                                                               placeholder=" ">
-                                                        <label>Email</label>
+                                                    <div class="form-group">
+                                                        <input type="email" name="email" id="login-email" value="{{ old('email') }}" class="floating-input form-control" placeholder="Enter your email address" required autocomplete="email" autofocus>
+
+                                                        @error('email')
+                                                        <div class="help-block text-warning">{{ $message }}</div>
+                                                        @enderror
                                                     </div>
                                                 </div>
                                                 <div class="col-lg-12">
-                                                    <div class="floating-label form-group">
-                                                        <input class="floating-input form-control" type="password"
-                                                               placeholder=" ">
-                                                        <label>Password</label>
+                                                    <div class="form-group">
+                                                        <input type="password" name="password" id="login-password" class="floating-input form-control" placeholder="Enter your Password" required autocomplete="current-password">
                                                     </div>
+
+                                                    @error('password')
+                                                    <div class="validation-invalid-label">{{ $message }}</div>
+                                                    @enderror
                                                 </div>
                                                 <div class="col-lg-6">
                                                     <div class="custom-control custom-checkbox mb-3">
-                                                        <input type="checkbox" class="custom-control-input"
-                                                               id="customCheck1">
-                                                        <label class="custom-control-label control-label-1 text-white"
-                                                               for="customCheck1">Remember Me</label>
+                                                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} class="custom-control-input">
+                                                        <label class="custom-control-label control-label-1 text-white" for="remember">Remember Me</label>
                                                     </div>
                                                 </div>
                                                 <div class="col-lg-6">
-                                                    <a href="auth-recoverpw.html" class="text-white float-right">Forgot Password?</a>
+                                                    @if(Route::has('password.request'))
+                                                    <a href="{{ route('password.request') }}" class="text-white float-right">Forgot Password?</a>
+                                                    @endif
                                                 </div>
                                             </div>
                                             <button type="submit" class="btn btn-white">Sign In</button>
@@ -45,8 +53,7 @@
                                     </div>
                                 </div>
                                 <div class="col-lg-6 content-right">
-                                    <img src="{{ secure_asset('assets/images/login/01.png') }}"
-                                         class="img-fluid image-right" alt="">
+                                    <img src="{{ secure_asset('assets/images/login/01.png') }}" class="img-fluid image-right" alt="">
                                 </div>
                             </div>
                         </div>
